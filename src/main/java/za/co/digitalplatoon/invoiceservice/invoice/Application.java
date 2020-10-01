58a8737eba3d054d4d5ea2a45966e3455eff99b1
William Gadney <WilliamGadney@eoh.com>
2020-10-02 01:17:14 +0200

    Initial commit

---
+++ src/main/java/za/co/digitalplatoon/invoiceservice/invoice/Application.java
@@ -0,0 +1,13 @@
+package za.co.digitalplatoon.invoiceservice.invoice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
